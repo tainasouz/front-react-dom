34462fd0e3071f934d01c8fb3b9344850c8d70f6
tainasouz <souzataina5657@gmail.com>
2023-04-12 08:14:25 -0300

    fix: Corrigir export de config

--- src/constants.js
+++ src/constants.js
@@ -5,4 +5,6 @@ const prod = {
 const dev = {
     url: 'http://localhost:3000/'
 };
-export const config = process.env.NODE_ENV === 'desenvolvimento' ? dev : prod;
+
+
+export const config = process.env.NODE_ENV === 'development' ? dev : prod;

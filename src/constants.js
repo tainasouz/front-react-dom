a3ac62aad1d11a5f3f3588651422ec8a895be2a8
tainasouz <souzataina5657@gmail.com>
2023-04-12 07:55:45 -0300

    fix: altera constante

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,12 @@
 
 const prod = {
-    url: 'https://api-filmes-hf8j.onrender.com/',
+    url: {
+        API_URL: 'https://api-filmes-hf8j.onrender.com/',
+    }
 };
 const dev = {
-    url: 'http://localhost:3000/'
+    url: {
+        API_URL: 'http://localhost:3000/'
+    }
 };
 export const config = process.env.NODE_ENV === 'desenvolvimento' ? dev : prod;

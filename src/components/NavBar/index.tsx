ddd68dcab92b81d65e922bf2c47dcc30d146d30d
tainasouz <souzataina5657@gmail.com>
2023-04-14 11:57:56 -0300

    reafctor: substitui "a href" por "Link to" pagina principal

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,6 +2,7 @@ import './style.css';
 import Logo from '../../assets/img/WatchMR.png'
 import { IconContext } from "react-icons";
 import { IoSearch } from 'react-icons/io5'
+import { Link } from 'react-router-dom';
 
 interface NavbarProps {
     color?: string;
@@ -12,14 +13,14 @@ function Navbar() {
     return (
         <div id="navbar">
             <div className="div-logo-menu">
-                <a href={'/'} className="logo">
+                <Link to="/" className="logo">
                     <img src={Logo} alt="" srcSet="" />
-                </a>
+                </Link>
 
             </div>
 
             <div className="div-pesquisa">
-                <form action="/pesquisa" method="get" className="pesquisa">
+                <form action='/pesquisa' method="get" className="pesquisa">
                     <input type="text" placeholder="Pesquisar" name="search" />
                     <button title="botao-pesquisa">
                         <IconContext.Provider value={{ style: { color: 'white', fontSize: '20px' } }}>
